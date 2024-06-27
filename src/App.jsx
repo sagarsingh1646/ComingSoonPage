1abbf40a1b57e086612b93570a75bf4ca9844f11
Sagar Singh <sagarsingh@Sagars-MacBook-Pro.local>
2024-06-27 11:10:35 +0530

    root path changed index.html

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 function App() {
   return (
     <div>
-    <p>Testing</p>
       <h1 className="text-3xl font-bold underline">WhisperOo</h1>
       <h3 className="text-3xl font-bold underline">Coming Soon!</h3>
     </div>
